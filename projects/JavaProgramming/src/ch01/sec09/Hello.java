8fc3c482d9e1ea6ce4f8d443edad5281d00339ad
gigomgigom <100766892+gigomgigom@users.noreply.github.com>
2024-03-01 19:57:39 +0900

    2024-03-01

--- projects/JavaProgramming/src/ch01/sec09/Hello.java
+++ projects/JavaProgramming/src/ch01/sec09/Hello.java
@@ -3,6 +3,21 @@ package ch01.sec09;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("dd");
+		int var1;
+		boolean flag = true;
+		
+		if(flag) {
+			int var2;
+			//var1, flag, var2 사용가능
+			//str 사용불가
+		}
+		
+		while(flag) {
+			String str;
+			//var1, flag, str 사용가능
+			//var2 사용불가
+		}
+		//var1, flag 사용가능
+		//var2, str 사용불가
 	}
 }
